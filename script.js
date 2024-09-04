e23877cfeb91e36723547b1c531120d65a1c669f
asthashaw <astha.shaw100@gmail.com>
2024-09-04 14:18:48 +0100

    Add first iteration of frontend code

---
+++ script.js
@@ -0,0 +1,21 @@
+const main=document.getElementById('main')
+const character=document.querySelector('.character')
+const searchInput=document.getElementById('search')
+
+let characterArray=[]
+
+getCharacters()
+
+
+function getCharacters(){
+    fetch("https://potterhead-api.vercel.app/api/characters")
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        showCharacters(data);
+    })
+}
+
+function showCharacters(data){
+    
+}
