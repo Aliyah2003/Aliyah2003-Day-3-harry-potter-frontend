57e798d12053368b3341932159ea6c561bb310e6
asthashaw <astha.shaw100@gmail.com>
2024-09-04 14:44:41 +0100

    Add the script.js loading characters

--- script.js
+++ script.js
@@ -17,5 +17,38 @@ function getCharacters(){
 }
 
 function showCharacters(data){
-    
+    main.innerHTML='';
+
+    characterArray=data.map(character=>{
+    const {name, image}=character;
+
+    const characterEl=document.createElement('div');
+    characterEl.classList.add('character');
+    characterEl.classList.add('front');
+
+    characterEl.innerHTML=
+    `<img src=${image} alt=${name} onerror="handleError(this);"/>
+    <div class="character-info">
+    <h3>${name}</h3>
+    </div>`
+
+    const characterBack=document.createElement('div');
+    characterBack.classList.add('character');
+    characterBack.classList.add('back');
+
+    characterBack.innerHTML=`<h1>Back of the card</h1>`
+
+    main.appendChild(characterEl);
+    main.appendChild(characterBack);
+    return {
+        name:character.name,
+        element:characterEl
+    }
+
+})
+
+}
+
+function handleError(imgElement){
+    imgElement.src="images/wizard-icon.jpeg"
 }
